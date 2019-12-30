95ad553d9891b5b4470b90d39ef8db4cf319b2e3
Narendra Yalavarthi <nyalavarthi@gmail.com>
2019-12-30 13:30:39 -0500

    Update sample-ec2.tf

--- sample-ec2.tf
+++ sample-ec2.tf
@@ -1,7 +1,7 @@
 # Create a new instance of the latest Ubuntu 14.04 on an
 # t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
 
 /*
