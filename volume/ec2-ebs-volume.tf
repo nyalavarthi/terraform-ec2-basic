13b26b51e71a80d89e5df07362f4b353447cb6d6
Narendra Yalavarthi <nyalavarthi@gmail.com>
2020-01-07 13:27:14 -0500

    Create ec2-ebs-volume.tf

---
+++ volume/ec2-ebs-volume.tf
@@ -0,0 +1,48 @@
+# Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_ami" "example" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+data "aws_ami_ids" "ubuntu-ids" {
+  owners = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["Ubuntu Server 18.04 LTS (HVM)"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-010fae13a16763bb4"
+  #ami           = "${data.aws_ami.amazon-linux-2.id}"
+  instance_type = "t2.large"
+  availability_zone = "eu-central-1b"
+
+  tags = {
+    Name        = "NY-test-EC2"
+    Environment = "Sandbox",
+    Owner       = "Narendra Yalavarthi"
+  }
+}
+
+output "ami_ids" {
+  value = "${data.aws_ami_ids.ubuntu-ids.*.ids}"
+}
+output "amiids" {
+  value = "${data.aws_ami.example.id}"
+}
